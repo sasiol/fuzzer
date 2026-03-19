e97a460ebdec6647fb746f4a21ae38219435d9f3
sasiol <sini.salmi@icloud.com>
2026-03-19 12:35:50 +0200

    working on mutating data

--- mutator.cpp
+++ mutator.cpp
@@ -1,2 +1,58 @@
 //store file to memory
 //flip, insert delete bytes
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+// Read file into memory
+std::vector<unsigned char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary);
+
+    if (!file) {
+        std::cerr << "Error opening file\n";
+        exit(1);
+    }
+
+    //Read all bytes into vector
+    std::vector<unsigned char> data(
+        (std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>()
+    );
+
+    return data;
+}
+
+// Mutate the data (flip random bytes)
+void mutate(std::vector<unsigned char>& data) {
+    if (data.empty()) return;
+
+    int numMutations = rand() % 10; // change to use random next
+
+    for (int i = 0; i < numMutations; i++) {
+        int index = rand() % data.size();   // pick random position
+        data[index] ^= 0xFF;      // flip 8 bits, better way to do this? ~ or std::bitset?
+    }
+}
+
+// Save mutated file
+void writeFile(const std::string& filename, const std::vector<unsigned char>& data) {
+    std::ofstream file(filename, std::ios::binary);
+    file.write((char*)data.data(), data.size());
+}
+
+int main() {
+    srand(time(0)); // seed randomness
+
+    auto data = readFile("seed/seed.txt");
+
+    mutate(data);
+
+    writeFile("mutated.bin", data);
+
+    std::cout << "Mutation done\n";
+
+    return 0;
+}
