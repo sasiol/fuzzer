480a9adf30b11610b78c1dec79920ab06f059a4f
sasiol <sini.salmi@icloud.com>
2026-04-02 21:42:04 +0300

    new mutations (bit flip, insert/erase byte)

--- mutator.cpp
+++ mutator.cpp
@@ -30,11 +30,32 @@ std::vector<unsigned char> readFile(const std::string& filename) {
 void mutate(std::vector<unsigned char>& data) {
     if (data.empty()) return;
 
-    int numMutations = rand() % 10; // change to use random next
+    int numMutations = rand() % 10 + 1; // change to use random next?
 
     for (int i = 0; i < numMutations; i++) {
+        int x = rand() % 4;
+
         int index = rand() % data.size();   // pick random position
-        data[index] ^= 0xFF;      // flip 8 bits, better way to do this? ~ or std::bitset?
+
+        switch(x){
+            case 0: //flip bit
+                data[index] ^= (1 << (rand()% 8));
+                break;
+            
+            case 1:  //flip a byte
+                 data[index] ^= 0xFF; 
+                 break;
+
+            case 2: //add byte
+                data.insert(data.begin() + index, rand() % 256);
+                break;
+
+            case 3: //delete byte
+                if (data.size() > 1)
+                data.erase(data.begin()+index);
+                break;
+        }
+       
     }
 }
 
